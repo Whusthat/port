b1357b226ec1236fea060a76af3800056e74fc77
Whusthat <mueller.sebastian117@gmail.com>
2022-03-23 20:56:08 +0100

    Clean Start

---
+++ public/components/Page-template.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+function PageTemplate(props) {
+  return (
+    <Head>
+      <title>{props.title}</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+  );
+}
+
+export default PageTemplate;
